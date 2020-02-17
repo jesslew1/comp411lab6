cc86a648cacad83c8b0e46d0efc5258f0bb97766
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-02-17 13:43:39 -0500

    Add files via upload

--- ex1.c
+++ ex1.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 // #include <math.h>
 
-int doubleToInt(double numb){
-    if(numb < 0) {
-        return (int)numb - 1;
-    }
-    else {
-        return (int)numb;
-    }
-}
+// int doubleToInt(double numb){
+//     if(numb < 0) {
+//         return (int)numb - 1;
+//     }
+//     else {
+//         return (int)numb;
+//     }
+// }
 int main(){
     int num;
-    double triangle = 0;
+    double triangle;
     // double floor(double number);
     // double ceil(double x);
     while(1){
@@ -19,16 +19,24 @@ int main(){
         scanf("%d", &num);
         for (int i = 1; i < num; i++){
             triangle +=i;
+            if (num == 0) {
+                printf("Done");
+                break;
+            } else if (num == triangle){
+                printf("%d is a triangular number \n", num);
+            } else if (num != triangle){
+                printf("%d is a not triangular number \n", num);
+            }
         }
         // double resultf = doubleToInt(triangle);
         // double resultc = ceil(triangle);
-        if (num == 0) {
-            printf("Done");
-            break;
-        } else if (triangle == num){
-            printf("%d is a triangular number \n", num);
-        } else if (triangle != num ){
-            printf("%d is a not triangular number \n", num);
-        }
+        // if (num == 0) {
+        //     printf("Done");
+        //     break;
+        // } else if (num == triangle){
+        //     printf("%d is a triangular number \n", num);
+        // } else if (num != triangle){
+        //     printf("%d is a not triangular number \n", num);
+        // }
     }
 }
