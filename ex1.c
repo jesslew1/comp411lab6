6c9499644bf4c5aaf5f9798355d78f2898fa4fed
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-02-17 14:23:30 -0500

    Add files via upload

--- ex1.c
+++ ex1.c
@@ -15,33 +15,37 @@
 // }
 int isTriangular(int num) { 
     // Base case 
-    if (num < 0) 
+    if (num < 0) {
         return 0; 
+    }
     // A Triangular number must be 
     // sum of first n natural numbers 
     int sum = 0; 
     for (int i = 1; sum <= num; i++) { 
         sum+=i; 
-        if (sum == num) 
+        if (sum == num) {
             return 1; 
+        }
     } 
     return 0; 
 } 
 int main(){
     int num;
-    double triangle;
+    // double triangle;
     // double floor(double number);
     // double ceil(double x);
     while(1) {
         printf("Number ?\n");
         scanf("%d", &num);
-        if (num == 0) {
+        if (num != 0) {
+            if (isTriangular(num) == 1){
+                printf("%d is a triangular number \n", num);
+            } else if (!isTriangular(num) == 0){
+                printf("%d is not a triangular number \n", num);
+            }
+        } else {
             printf("Done");
             break;
-        } else if (isTriangular(num) == 1){
-            printf("%d is a triangular number \n", num);
-        } else if (!isTriangular(num) == 0){
-            printf("%d is not a triangular number \n", num);
         }
         // for (int i = 1; i < num; i++){
         //     triangle +=i;
