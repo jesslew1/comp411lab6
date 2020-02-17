73684223afa0978dd5801b6cedeee3b1b7821084
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-02-17 15:00:24 -0500

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int N;                    // length of pattern
+char pattern[11];         // at most 20 chars plus NULL
+
+void makepatterns(int currentlevel) {
+  if(currentlevel == N)
+    printf("%s", pattern);    // print a pattern
+  else {
+    pattern[currentlevel] = '0';       // set one character to something
+    makepatterns(currentlevel + 1);      // recursive call
+    pattern[currentlevel] = '1';       // set it to something else
+    makepatterns(currentlevel + 1);      // recursive call
+  } 
+}
+
+int main() {
+  scanf("%d", &N);        // read N, pattern length
+  pattern[N] = '\0';      // terminate string at position N
+  makepatterns(0);        // generate all patterns (recursively)
+}
