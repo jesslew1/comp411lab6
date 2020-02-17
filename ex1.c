564e11411bd2691eee3b5e29ace0d4f905437064
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-02-17 13:00:39 -0500

    Add files via upload

--- ex1.c
+++ ex1.c
@@ -3,18 +3,20 @@
 
 int main(){
     int num;
-    float triangular;
+    double triangle;
     // double floor(double x);
+
     while(1){
         printf("Number ?\n");
         scanf("%d", &num);
-        triangular = (num*(num+1)) / 2;
+        triangle = (num*(num+1)) / 2;
+        result = floor(triangle);
         if (num == 0) {
             printf("done");
             break;
-        } else if (triangular == floor(triangular)){
+        } else if (triangle == result){
             printf("%d is a triangular number \n", num);
-        } else {
+        } else if (triangle != result){
             printf("%d is a not triangular number \n", num);
         }
     }
