e5891dcb69eda8679ab0d5c844ac848f07993322
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-02-17 14:30:19 -0500

    Add files via upload

--- ex1.c
+++ ex1.c
@@ -38,10 +38,11 @@ int main(){
         printf("Number ?\n");
         scanf("%d", &num);
         if (num != 0) {
-            if (isTriangular(num) != 0){
-                printf("%d is a triangular number", num);
-            } else if (!isTriangular(num) == 0){
+            if (!isTriangular(num) == 0){
                 printf("%d is not a triangular number\n", num);
+            }
+            } else if (isTriangular(num) == 1){
+                printf("%d is a triangular number\n", num);
             }
         } else if (num ==0){
             printf("Done");
